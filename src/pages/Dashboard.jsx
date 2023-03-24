fde10489e7371065879b4f1c6912aa45a77a2878
Lo <ryan.lo@fairfield.edu>
2023-03-24 18:41:01 -0400

    Routing basics and loaders

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import { fetchData } from "../helpers";
+
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
+
+const Dashboard = () => {
+  const { userName } = useLoaderData();
+  return (
+    <div>
+      <h1>{userName}</h1>
+      Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
